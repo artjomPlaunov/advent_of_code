114454fcacf4ebb3c5611fb07ef5acf006594981
Artjom Plaunov <artyemnyc@gmail.com>
2024-12-01 00:28:56 -0500

    2024 day 1

--- 2024/day1.go
+++ 2024/day1.go
@@ -1,28 +1,48 @@
 // day1.go
 package main
 
-import "strconv"
+import (
+	"bytes"
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+)
 
 func day1(input []byte) (string, string) {
-	floor := 0
-	firstBasement := 0
-	foundBasement := false
-
-	// Process each character
-	for i, b := range input {
-		if b == '(' {
-			floor++
-		} else {
-			floor--
+	var list1, list2 []int
+	lines := bytes.Split(input, []byte{'\n'})
+	for _, line := range lines {
+		if len(bytes.TrimSpace(line)) == 0 {
+			continue
 		}
-
-		// Check for first basement entry (part 2)
-		if floor < 0 && !foundBasement {
-			firstBasement = i + 1
-			foundBasement = true
-		}
+		parts := bytes.Fields(line)
+		num1, _ := strconv.Atoi(string(parts[0]))
+		num2, _ := strconv.Atoi(string(parts[1]))
+		list1 = append(list1, num1)
+		list2 = append(list2, num2)
+	}
+	var distance int
+	sort.Ints(list1)
+	sort.Ints(list2)
+	for i := 0; i < len(list1); i++ {
+		n1 := list1[i]
+		n2 := list2[i]
+		distance += int(math.Abs(float64(n1) - float64(n2)))
+	}
+	freq1 := make(map[int]int)
+	freq2 := make(map[int]int)
+	for _, num := range list1 {
+		freq1[num] += 1
+	}
+	for _, num := range list2 {
+		freq2[num] += 1
+	}
+	similarity := 0
+	for _, k := range list1 {
+		similarity += (k * freq2[k])
 	}
 
-	// Convert results to strings and return
-	return strconv.Itoa(floor), strconv.Itoa(firstBasement)
+	return fmt.Sprintf("%d", distance), fmt.Sprintf("%d", similarity)
+
 }
