6fe4a72dbde8fee6459694a29553f7b88b602891
Artjom Plaunov <artyemnyc@gmail.com>
2024-12-04 13:01:55 -0500

    day4 part 2

--- 2024/day4.go
+++ 2024/day4.go
@@ -42,11 +42,21 @@ func matches(i, j int, grid [][]byte, word []byte) int {
 func day4(input []byte) (string, string) {
 	lines := bytes.Split(input, []byte{'\n'})
 	res := 0
+	part2 := 0
 	n, m := len(lines), len(lines[0])
 	for i := range n {
 		for j := range m {
 			res += matches(i, j, lines, []byte("XMAS"))
+			if lines[i][j] == 'A' {
+				if i-1 >= 0 && i+1 < n && j-1 >= 0 && j+1 < m {
+					if (lines[i-1][j-1] == 'M' && lines[i+1][j+1] == 'S') || (lines[i-1][j-1] == 'S' && lines[i+1][j+1] == 'M') {
+						if (lines[i-1][j+1] == 'M' && lines[i+1][j-1] == 'S') || (lines[i-1][j+1] == 'S' && lines[i+1][j-1] == 'M') {
+							part2 += 1
+						}
+					}
+				}
+			}
 		}
 	}
-	return fmt.Sprintf("%d", res), ""
+	return fmt.Sprintf("%d", res), fmt.Sprintf("%d", part2)
 }
