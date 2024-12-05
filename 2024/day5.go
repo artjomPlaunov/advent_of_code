50fbedaabde2a4764570e07181dd8577092c6be9
Artjom Plaunov <artyemnyc@gmail.com>
2024-12-05 18:40:58 -0500

    2024 day5 part 2

--- 2024/day5.go
+++ 2024/day5.go
@@ -9,6 +9,11 @@ import (
 	"strings"
 )
 
+type T struct {
+	A int
+	B int
+}
+
 func isValidPair(order [][]int, n1, n2 int) bool {
 	fmt.Println(n1, n2)
 	for _, o := range order {
@@ -30,8 +35,20 @@ func isValid(order [][]int, update []int) bool {
 	return true
 }
 
-func rearrange(order [][]int, update []int) int {
-	return 0
+func rearrange(order map[T]bool, update []int) int {
+	changed := true
+	for changed {
+		changed = false
+		for i := range update {
+			for j := i + 1; j < len(update); j++ {
+				if order[T{update[j], update[i]}] {
+					update[i], update[j] = update[j], update[i]
+					changed = true
+				}
+			}
+		}
+	}
+	return update[len(update)/2]
 }
 
 func day5(input []byte) (string, string) {
@@ -60,13 +77,18 @@ func day5(input []byte) (string, string) {
 	}
 	p1 := 0
 	p2 := 0
+	orderMap := make(map[T]bool)
+	for _, o := range order {
+		orderMap[T{o[0], o[1]}] = true
+	}
+	fmt.Println(orderMap)
 	for _, u := range updates {
 		if isValid(order, u) {
 			p1 += u[len(u)/2]
 		} else {
-			p2 += rearrange(order, u)
+			p2 += rearrange(orderMap, u)
 		}
 	}
 
-	return fmt.Sprintf("%d", p1), ""
+	return fmt.Sprintf("%d", p1), fmt.Sprintf("%d", p2)
 }
