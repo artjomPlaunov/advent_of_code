72bd1a32d57275b1dad3925e7a92e5eb0c0c53d8
Artjom Plaunov <artyemnyc@gmail.com>
2024-12-16 21:11:13 -0500

    day16 -- caught up again!

--- day16.go
+++ day16.go
@@ -42,13 +42,13 @@ func day16(input []byte) (string, string) {
 	}
 	lines := bytes.Split(input, []byte{'\n'})
 	m, n := len(lines), len(lines[0])
-
+	maxDist := 99999999999999
 	minDists := make(map[T]map[int]int)
 	for i := range m {
 		for j := range n {
 			minDists[T{i, j}] = make(map[int]int)
 			for k := range 4 {
-				minDists[T{i, j}][k] = 99999999999999
+				minDists[T{i, j}][k] = maxDist
 			}
 		}
 	}
@@ -59,12 +59,14 @@ func day16(input []byte) (string, string) {
 	heap.Init(h)
 	heap.Push(h, Node{s.A, s.B, 1, 0})
 	minDists[T{s.A, s.B}][1] = 0
-	p1 := 0
+	p1 := maxDist
 	for h.Len() > 0 {
 		u := heap.Pop(h).(Node)
 		if u.A == e.A && u.B == e.B {
-			p1 = minDists[T{u.A, u.B}][u.Dir]
-			break
+			if minDists[T{u.A, u.B}][u.Dir] < p1 {
+				p1 = minDists[T{u.A, u.B}][u.Dir]
+			}
+			continue
 		}
 		if minDists[T{u.A, u.B}][u.Dir] < u.Dist {
 			continue
@@ -92,8 +94,51 @@ func day16(input []byte) (string, string) {
 				}
 			}
 		}
-
+	}
+	visited := make(map[T]map[int]bool)
+	marked := make(map[T]bool)
+	for i := range m {
+		for j := range n {
+			visited[T{i, j}] = make(map[int]bool)
+		}
+	}
+	stack := make([][]int, 0)
+	stack = append(stack, []int{e.A, e.B, 0, 0})
+	for len(stack) > 0 {
+		p := stack[len(stack)-1]
+		x, y, d, dist := p[0], p[1], p[2], p[3]
+		stack = stack[:len(stack)-1]
+		visited[T{x, y}][d] = true
+		minDist := minDists[T{x, y}][d]
+		if minDist == maxDist {
+			continue
+		}
+		if minDist+dist == p1 {
+			marked[T{x, y}] = true
+		} else {
+			continue
+		}
+		for _, i := range []int{(d + 1) % 4, (d - 1) % 4, d} {
+			if i < 0 {
+				i = 3
+			}
+			if i == d {
+				x, y = x-dirs[d][0], y-dirs[d][1]
+				if !visited[T{x, y}][d] {
+					stack = append(stack, []int{x, y, d, dist + 1})
+				}
+			} else {
+				if !visited[T{x, y}][i] {
+					stack = append(stack, []int{x, y, i, dist + 1000})
+				}
+			}
+		}
+	}
+	p2 := 0
+	for range marked {
+		p2 += 1
 	}
 
-	return fmt.Sprintf("%d", p1), ""
+	fmt.Println(minDists[T{e.A, e.B}])
+	return fmt.Sprintf("%d", p1), fmt.Sprintf("%d", p2)
 }
