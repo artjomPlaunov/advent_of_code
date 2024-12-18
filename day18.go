54142b855c8b2859698cf133e9156fb77e7c1db1
Artjom Plaunov <artyemnyc@gmail.com>
2024-12-18 12:02:42 -0500

    day18!

--- day18.go
+++ day18.go
@@ -38,22 +38,9 @@ func (h *NH) Pop() interface{} {
 	return x
 }
 
-func day18(input []byte) (string, string) {
+func dijkstra(bytes map[T]bool) int {
 	dirs := [][]int{
 		{-1, 0}, {0, 1}, {1, 0}, {0, -1},
-	}
-	lines := bytes.Split(input, []byte{'\n'})
-	bytes := make(map[T]bool)
-	for i, l := range lines {
-		if i >= 1024 {
-			break
-		}
-		nums := strings.Split(string(l), ",")
-		n1, _ := strconv.Atoi(nums[0])
-		n2, _ := strconv.Atoi(nums[1])
-		bytes[T{n2, n1}] = true
-		fmt.Println(n2, n1)
-
 	}
 	n := 71
 	maxDist := math.MaxInt
@@ -71,22 +58,22 @@ func day18(input []byte) (string, string) {
 
 	heap.Push(h, N{0, 0, 0})
 	minDists[T{0, 0}] = 0
-	p1 := maxDist
-	for i := range n {
-		for j := range n {
-			if _, ok := bytes[T{i, j}]; ok {
-				fmt.Print(string('#'))
-			} else {
-				fmt.Print(string('.'))
-			}
-		}
-		fmt.Println()
-	}
+	res := maxDist
+	// for i := range n {
+	// 	for j := range n {
+	// 		if _, ok := bytes[T{i, j}]; ok {
+	// 			fmt.Print(string('#'))
+	// 		} else {
+	// 			fmt.Print(string('.'))
+	// 		}
+	// 	}
+	// 	fmt.Println()
+	// }
 	for h.Len() > 0 {
 		u := heap.Pop(h).(N)
 		if u.X == n-1 && u.Y == n-1 {
-			p1 = minDists[T{n - 1, n - 1}]
-			break
+			res = minDists[T{n - 1, n - 1}]
+			return res
 		}
 		if minDists[T{u.X, u.Y}] < u.Dist {
 			continue
@@ -103,6 +90,28 @@ func day18(input []byte) (string, string) {
 			}
 		}
 	}
+	return res
+}
 
-	return fmt.Sprintf("%d", p1), ""
+func day18(input []byte) (string, string) {
+
+	lines := bytes.Split(input, []byte{'\n'})
+	p2 := T{0, 0}
+	bytes := make(map[T]bool)
+	for _, l := range lines {
+
+		nums := strings.Split(string(l), ",")
+		n1, _ := strconv.Atoi(nums[0])
+		n2, _ := strconv.Atoi(nums[1])
+		bytes[T{n2, n1}] = true
+		if dijkstra(bytes) == math.MaxInt {
+			p2 = T{n1, n2}
+			break
+		}
+		//fmt.Println(n2, n1)
+	}
+	bytes = make(map[T]bool)
+	p1 := dijkstra(bytes)
+
+	return fmt.Sprintf("%d", p1), fmt.Sprintf("%d,%d", p2.A, p2.B)
 }
